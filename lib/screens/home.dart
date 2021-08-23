72b9286d28b20c9ca5eb1cc92bd576eebad85fee
ayaadelhassan <ayaadelhassandarweesh@gmail.com>
2021-08-23 22:45:35 +0200

    Splash Screen done

---
+++ lib/screens/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: const Color(0xff1F2121),
+        child: Center(
+        )
+      )
+    );
+  }
+}
